28e833d8d9fef795aa2a44657b03806f4f3748df
Dallin Sprague <61210689+DSprague12@users.noreply.github.com>
2025-05-15 15:18:38 -0600

    Fixed class and string initializations and fixed silly typos

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -1,4 +1,4 @@
-public class Journal(){
+public class Journal{
     public List<string[]> journalStage = new List<string[]>();
 
     //Loads journal from the file and puts it in the cleared "staging area"
@@ -6,7 +6,7 @@ public class Journal(){
         string[] lines = System.IO.File.ReadAllLines(fileName);
         journalStage = new List<string[]>();
         foreach (string line in lines){
-            journalStage.Add(deobsfucate(line));
+            journalStage.Add(deobfucate(line));
         }
     }
 
@@ -25,15 +25,15 @@ public class Journal(){
     public void saveJournal(string fileName){
         using(StreamWriter finalFile = new StreamWriter(fileName)){
             foreach (string[] item in journalStage){
-                finalFile.WriteLine($"{obsfucate(item[0])},{obsfucate(item[1])},{obsfucate(item[2])}");
+                finalFile.WriteLine($"{obfucate(item[0])},{obfucate(item[1])},{obfucate(item[2])}");
             }
         }
     }
 
     //Converts from bit64 to plaintext ;)
-    string[] deobsfucate(string data){
+    string[] deobfucate(string data){
         string[] splitData = data.Split(",");
-        string[] goodData = ["","",""];
+        string[] goodData = new string[] {"","",""};
         for(int i=0; i<3; i++){
             var tempData = System.Convert.FromBase64String(splitData[i]);
             goodData[i] = (System.Text.Encoding.UTF8.GetString(tempData));
@@ -42,7 +42,7 @@ public class Journal(){
     }
 
     //Converts from plaintext to bit64 (stretch challenge? it resolves the issue with commas in the csv file)
-    public string obsfucate(string plainText){
+    public string obfucate(string plainText){
         var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
         return System.Convert.ToBase64String(plainTextBytes);
     }
--- prove/Develop02/Journal_Entry.cs
+++ prove/Develop02/Journal_Entry.cs
@@ -1,11 +1,11 @@
-public class Entry(){
+public class Entry{
     public string date;
     public string prompt;
     public string userEntry;
 
     //Returns what the entry output will be
     public string[] fullEntry(){
-        string[] output = [date,prompt,userEntry];
+        string[] output = new string[] {date, prompt, userEntry};
         return output;    
     }
 
